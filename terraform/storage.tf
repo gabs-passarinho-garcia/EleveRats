274748b8974792726027603f863f811f52d0d2ae
Gabriel Passarinho Garcia <gabrieldefreitasgarcia@outlook.com>
2026-04-05 15:50:37 -0300

    feat: configure R2 custom domain in Terraform and update hero image source to use new CDN URL

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,3 +4,11 @@ resource "cloudflare_r2_bucket" "assets" {
   location   = "ENAM" # Choose an appropriate location
 }
 
+resource "cloudflare_r2_custom_domain" "assets_public_domain" {
+  account_id  = var.cloudflare_account_id
+  bucket_name = cloudflare_r2_bucket.assets.name
+  domain      = var.r2_custom_domain
+  zone_id     = var.cloudflare_zone_id
+  enabled     = true
+}
+
